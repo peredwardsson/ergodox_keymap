525040fe6a986eb761f6ef7168a6682797a9bfa4
Per Edwardsson <per@hugodelivery.com>
2023-05-27 20:07:55 +0200

    Added combo aaaand did some other changes

--- keymap.c
+++ keymap.c
@@ -15,18 +15,117 @@
  */
 
 #include QMK_KEYBOARD_H
+#include "action_layer.h"
 #include "action_tapping.h"
 #include "debug.h"
-#include "action_layer.h"
-#include "keymap_swedish.h"
-#include "sendstring_swedish.h"
+#include "keymap_swedish_mac_iso.h"
+// #include "sendstring_swedish.h"
+// #include "sendstring_swedish_mac_iso.h"
+
+#include "quantum.h"
+
+// clang-format off
+
+const uint8_t ascii_to_shift_lut[16] PROGMEM = {
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 1, 1, 1, 0, 1, 1, 0),
+    KCLUT_ENTRY(1, 1, 1, 0, 0, 0, 0, 1),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 1, 1, 0, 1, 1, 1),
+    KCLUT_ENTRY(0, 1, 1, 1, 1, 1, 1, 1),
+    KCLUT_ENTRY(1, 1, 1, 1, 1, 1, 1, 1),
+    KCLUT_ENTRY(1, 1, 1, 1, 1, 1, 1, 1),
+    KCLUT_ENTRY(1, 1, 1, 0, 0, 0, 1, 1),
+    KCLUT_ENTRY(1, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0)
+};
+
+const uint8_t ascii_to_altgr_lut[16] PROGMEM = {
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+
+    KCLUT_ENTRY(0, 0, 0, 0, 1, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(1, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 1, 1, 1, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 1, 1, 1, 1, 0)
+};
+
+const uint8_t ascii_to_dead_lut[16] PROGMEM = {
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 1, 0),
+    KCLUT_ENTRY(1, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 0, 0),
+    KCLUT_ENTRY(0, 0, 0, 0, 0, 0, 1, 0)
+};
+
+const uint8_t ascii_to_keycode_lut[128] PROGMEM = {
+    // NUL   SOH      STX      ETX      EOT      ENQ      ACK      BEL
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    // BS    TAB      LF       VT       FF       CR       SO       SI
+    KC_BSPC, KC_TAB,  KC_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    // DLE   DC1      DC2      DC3      DC4      NAK      SYN      ETB
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    // CAN   EM       SUB      ESC      FS       GS       RS       US
+    XXXXXXX, XXXXXXX, XXXXXXX, KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+
+    //       !        "        #        $        %        &        '
+    KC_SPC,  SE_1,    SE_2,    SE_3,    SE_4,    SE_5,    SE_6,    SE_QUOT,
+    // (     )        *        +        ,        -        .        /
+    SE_8,    SE_9,    SE_QUOT, SE_PLUS, SE_COMM, SE_MINS, SE_DOT,  SE_7,
+    // 0     1        2        3        4        5        6        7
+    SE_0,    SE_1,    SE_2,    SE_3,    SE_4,    SE_5,    SE_6,    SE_7,
+    // 8     9        :        ;        <        =        >        ?
+    SE_8,    SE_9,    SE_DOT,  SE_COMM, SE_LABK, SE_0,    SE_LABK, SE_PLUS,
+    // @     A        B        C        D        E        F        G
+    SE_2,    SE_A,    SE_B,    SE_C,    SE_D,    SE_E,    SE_F,    SE_G,
+    // H     I        J        K        L        M        N        O
+    SE_H,    SE_I,    SE_J,    SE_K,    SE_L,    SE_M,    SE_N,    SE_O,
+    // P     Q        R        S        T        U        V        W
+    SE_P,    SE_Q,    SE_R,    SE_S,    SE_T,    SE_U,    SE_V,    SE_W,
+    // X     Y        Z        [        \        ]        ^        _
+    SE_X,    SE_Y,    SE_Z,    SE_8,    SE_PLUS, SE_9,    SE_DIAE, SE_MINS,
+    // `     a        b        c        d        e        f        g
+    SE_ACUT, SE_A,    SE_B,    SE_C,    SE_D,    SE_E,    SE_F,    SE_G,
+    // h     i        j        k        l        m        n        o
+    SE_H,    SE_I,    SE_J,    SE_K,    SE_L,    SE_M,    SE_N,    SE_O,
+    // p     q        r        s        t        u        v        w
+    SE_P,    SE_Q,    SE_R,    SE_S,    SE_T,    SE_U,    SE_V,    SE_W,
+    // x     y        z        {        |        }        ~        DEL
+    SE_X,    SE_Y,    SE_Z,    SE_7,    SE_LABK, SE_0,    SE_DIAE, KC_DEL
+};
 
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
 #define PROG 3 // programming keys
 #define VIM  4 // vim like keys
-
 #define TAPPING_TERM_PER_KEY
 
 // #undef TAPPING_TOGGLE
@@ -72,8 +171,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
     // left hand
     KC_ESC,         KC_1,     KC_2,     KC_3,    KC_4,    KC_5,   KC_F2,
-    LT(VIM, KC_TAB),          KC_Q,     KC_W,     KC_E,    LGUI_T(KC_R) ,    KC_T,   MO(SYMB),
-    MO(PROG),        KC_A,     LALT_T(KC_S),     LCTL_T(KC_D),    LT(PROG,KC_F),    KC_G,
+    KC_TAB,          KC_Q,     KC_W,     KC_E,    KC_R ,    KC_T,   MO(SYMB),
+    MO(PROG),        KC_A,     LALT_T(KC_S),     KC_D,    KC_F,    KC_G,
     KC_LSFT,         KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,                    TT(VIM),
     KC_LCTRL,  SE_CIRC,  SE_ASTR,  KC_LALT, KC_LGUI,
                                                KC_LCTRL,  KC_LALT,
@@ -81,14 +180,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                          LT(SYMB, KC_ENTER), KC_ENTER, SE_BSLS,
     // right hand
 
-         PE_TILD,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,     KC_BSPC,
-         KC_ENTER,  KC_Y,   LGUI_T(KC_U), KC_I,    KC_O,    KC_P,     SE_ARNG,
-                    KC_H,   LT(PROG, KC_J), RCTL_T(KC_K),    RALT_T(KC_L),    SE_ODIA,  SE_ADIA,
-         KC_DEL,    KC_N,   LT(MDIA, KC_M)       , KC_COMM, KC_DOT,  SE_MINS,  KC_RSFT,
-                            MO(PROG) , SE_PIPE, SE_AT, SE_PLUS,  SE_QUOT,
+         SE_TILD,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,     KC_BSPC,
+         KC_ENTER,  KC_Y,   KC_U, KC_I,    KC_O,    KC_P,     SE_ARNG,
+                    KC_H,   KC_J, KC_K,    RALT_T(KC_L),    SE_ODIA,  SE_ADIA,
+         TO(SYMB),    KC_N,   LT(MDIA, KC_M)       , KC_COMM, KC_DOT,  SE_MINS,  KC_RSFT,
+                            KC_RGUI , KC_RALT, KC_LGUI, SE_PLUS,  SE_QUOT,
          KC_HOME,        KC_END,
          KC_PGUP,
-         KC_PGDN, CTL_T(KC_TAB), LT(SYMB, KC_SPACE)
+         KC_PGDN, CTL_T(KC_TAB), KC_SPACE
 ),
 
 /* Keymap 1: Symbol Layer
@@ -116,21 +215,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = LAYOUT_ergodox(
        // left hand
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_TRNS,
+       KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   LCTL(KC_UP), KC_TRNS,
+       KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   LCTL(KC_DOWN),
        KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                        KC_TRNS,KC_TRNS,
                                                KC_TRNS,
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_PSLS, KC_PAST, KC_PMNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_PPLS, KC_TRNS,
-                KC_TRNS, KC_4,    KC_5,    KC_6,    KC_PPLS, KC_TRNS,
-       RESET,   KC_DOT, KC_1,    KC_2,    KC_3,    KC_UP , KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       TO(BASE), LALT(KC_UP), KC_7,    KC_8,    KC_9,    KC_PPLS, KC_TRNS,
+                LALT(KC_DOWN), KC_4,    KC_5,    KC_6,    KC_PPLS, KC_TRNS,
+       RESET,   KC_TRNS, KC_1,    KC_2,    KC_3,    KC_UP , KC_TRNS,
                          KC_0,    KC_COMM, KC_LEFT ,  KC_DOWN, KC_RIGHT,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
+       SE_GRV, SE_ACUT,
+       SE_DIAE,
        KC_TRNS, KC_TRNS, KC_TRNS
 
 ),
@@ -199,7 +298,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // PROGRAMMUNICATION
 [PROG] = LAYOUT_ergodox(
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, SE_ASTR,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, SE_SLSH, SE_BSLS, SE_ASTR,
        SE_DQUO, KC_COPY, KC_PSTE, SE_LPRN, SE_RPRN, SE_EQL, SE_MINS,
        SE_QUOT, PE_GRAV, KC_CUT, SE_LBRC, SE_RBRC, SE_COLN,
        KC_TRNS, SE_HASH, PE_RARR, SE_LCBR, SE_RCBR, SE_SCLN, SE_UNDS,
@@ -209,9 +308,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_TRNS, KC_TRNS, KC_TRNS,
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, TO(VIM), TO(BASE), KC_TRNS,
-       KC_TRNS,  KC_TRNS, SE_LCBR, SE_RCBR, SE_EXLM, SE_QUES, KC_TRNS,
+       KC_TRNS,  SE_PIPE, SE_LCBR, SE_RCBR, SE_EXLM, SE_QUES, SE_DLR,
                  SE_SCLN, SE_LABK, SE_RABK, SE_HASH, SE_PERC, SE_AMPR,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, SE_MINS, SE_PLUS, SE_CIRC, KC_TRNS, KC_TRNS,
                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
@@ -252,11 +351,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,  KC_TRNS, KC_PGUP, KC_TRNS, TO(VIM), TO(BASE), KC_TRNS,
        KC_TRNS,  KC_PGUP, KC_HOME, KC_END, KC_PGDN, KC_TRNS, KC_TRNS,
                  KC_LEFT, KC_DOWN, KC_UP , KC_RIGHT, KC_TRNS, KC_TRNS,
-       KC_BSPC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_ENTER,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
-       KC_TRNS, KC_LSHIFT, KC_LGUI
+       KC_TRNS, KC_TAB, KC_SPACE
 ),
 };
 
@@ -264,6 +363,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 void matrix_init_user(void) {
 
 }
+
+// Combos
+enum combos {
+    QW_ESC,
+    YU_LPAREN,
+    UI_RPAREN,
+    HJ_LBRACE,
+    JK_RBRACE,
+    ER_LBRACKET,
+    RT_RBRACKET,
+    DF_LANGLE,
+    FG_RANGLE,
+    COMBO_LENGTH
+};
+uint16_t COMBO_LEN = COMBO_LENGTH;
+const uint16_t PROGMEM combo_qw_esc[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM combo_yu_lparen[] = {KC_Y, KC_U, COMBO_END};
+const uint16_t PROGMEM combo_ui_rparen[] = {KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM combo_hj_lbracket[] = {KC_H, KC_J, COMBO_END};
+const uint16_t PROGMEM combo_jk_rbracket[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM combo_er_lbracket[] = {KC_E, KC_R, COMBO_END};
+const uint16_t PROGMEM combo_rt_rbracket[] = {KC_R, KC_T, COMBO_END};
+const uint16_t PROGMEM combo_df_langle[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM combo_fg_rangle[] = {KC_F, KC_G, COMBO_END};
+
+combo_t key_combos[] = {
+    [QW_ESC] = COMBO(combo_qw_esc, KC_ESC),
+    [YU_LPAREN] = COMBO(combo_yu_lparen, SE_LPRN),
+    [UI_RPAREN] = COMBO(combo_ui_rparen, SE_RPRN),
+    [HJ_LBRACE] = COMBO(combo_hj_lbracket, SE_LCBR),
+    [JK_RBRACE] = COMBO(combo_jk_rbracket, SE_RCBR),
+    [ER_LBRACKET] = COMBO(combo_er_lbracket, SE_LBRC),
+    [RT_RBRACKET] = COMBO(combo_rt_rbracket, SE_RBRC),
+    [DF_LANGLE] = COMBO(combo_df_langle, SE_LABK),
+    [FG_RANGLE] = COMBO(combo_fg_rangle, SE_RABK),
+};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     mod_state = get_mods();
@@ -279,26 +414,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING("`");
         }
         return false;
-    case PE_LARR:
-        if (record->event.pressed) {
-            SEND_STRING("<-");
-        }
-        return false;
-    case PE_RARR:
-        if (record->event.pressed) {
-            SEND_STRING("->");
-        }
-        return false;
-    case PE_LEAR:
-        if (record->event.pressed) {
-            SEND_STRING("<=");
-        }
-        return false;
-    case PE_REAR:
-        if (record->event.pressed) {
-            SEND_STRING("=>");
-        }
-        return false;
+    // case PE_LARR:
+    //     if (record->event.pressed) {
+    //         SEND_STRING("<-");
+    //     }
+    //     return false;
+    // case PE_RARR:
+    //     if (record->event.pressed) {
+    //         SEND_STRING("->");
+    //     }
+    //     return false;
+    // case PE_LEAR:
+    //     if (record->event.pressed) {
+    //         SEND_STRING("<=");
+    //     }
+    //     return false;
+    // case PE_REAR:
+    //     if (record->event.pressed) {
+    //         SEND_STRING("=>");
+    //     }
+    //     return false;
     default:
         return true;
   }
